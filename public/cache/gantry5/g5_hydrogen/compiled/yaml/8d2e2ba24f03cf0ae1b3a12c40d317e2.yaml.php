3c4a788f0ab282d99721aff36beb739cae21c90c
Alex Crawford <awc737@gmail.com>
2016-01-30 01:31:51 -0800

    remove cache

--- public/cache/gantry5/g5_hydrogen/compiled/yaml/8d2e2ba24f03cf0ae1b3a12c40d317e2.yaml.php
+++
@@ -1,9 +0,0 @@
-<?php
-return [
-    '@class' => 'Gantry\\Component\\File\\CompiledYamlFile',
-    'filename' => '/var/www/jmg/site/public/templates/g5_hydrogen/custom/config/20/particles/sample.yaml',
-    'modified' => 1454140449,
-    'data' => [
-        'description' => ''
-    ]
-];
--- public/cache/gantry5/g5_hydrogen/compiled/yaml/980d89f0effa782360ba611115c3ddcb.yaml.php
+++
@@ -1,9 +0,0 @@
-<?php
-return [
-    '@class' => 'Gantry\\Component\\File\\CompiledYamlFile',
-    'filename' => '/var/www/justmakegames.com/public/templates/g5_hydrogen/custom/config/20/particles/copyright.yaml',
-    'modified' => 1454144806,
-    'data' => [
-        'enabled' => '1'
-    ]
-];
